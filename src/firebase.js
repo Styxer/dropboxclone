d27a947e92b7c59afa7c4adbdc29d14fada9a729
ofir rosner <gennin@gmail.com>
2021-10-13 12:02:54 +0300

    login and logout

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+
+
+const app = firebase.initializeApp(
+{
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PORJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID   
+
+})
+
+export const auth = firebase.auth();
+export default app
+
+
+
